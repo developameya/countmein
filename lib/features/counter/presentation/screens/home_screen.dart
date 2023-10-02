86c9aff875db64bba5dddf683fde70ab0d4830ef
Ameya Bhagat <developameya@outlook.com>
2023-10-02 16:12:49 +0530

    create home screen UI.

---
+++ lib/features/counter/presentation/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'package:beamer/beamer.dart';
+import 'package:count_me_in/features/core/common/constants.dart';
+import 'package:count_me_in/features/counter/presentation/screens/count_buttons_screen.dart';
+import 'package:flutter/material.dart';
+
+///Screen with the counter.
+///
+///Provides buttons to increase or decrease the count.
+///
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  static const routeName = '/';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter != 0) {
+        _counter--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(appTitle),
+        actions: [
+          IconButton(
+            onPressed: () {
+              //TODO: navigate to buttons screen
+              context.beamToNamed(CountButtonsScreen.routeName);
+            },
+            icon: const Icon(Icons.arrow_circle_right_rounded),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(countPrompt),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              heroTag: null,
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              heroTag: null,
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
