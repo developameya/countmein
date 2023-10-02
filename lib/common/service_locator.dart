0858b0b1c73e40760c1ecee2fb7f8280f8d0610f
Ameya Bhagat <developameya@outlook.com>
2023-10-02 17:43:03 +0530

    add docs for sl.

--- lib/common/service_locator.dart
+++ lib/common/service_locator.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'service_locator.config.dart';
 
+///Service locator for the app.
+///
 final sl = GetIt.instance;
 
 @InjectableInit(
