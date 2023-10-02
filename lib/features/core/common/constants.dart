6f1710756b33791d28da1a72dd8b2ab6007c50dd
Ameya Bhagat <developameya@outlook.com>
2023-10-02 16:12:26 +0530

    add constants for the app.

---
+++ lib/features/core/common/constants.dart
@@ -0,0 +1,5 @@
+const appTitle = 'Count Me In';
+const countPrompt = 'What\'s the Count?';
+const buttonScreenTitle = 'Big Buttons!';
+const increamentButtonLabel = 'Increase';
+const decreamentButtonLabel = 'Decrease';
