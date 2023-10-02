87df488649e7bdc29c0dec0fe492f821cfc04f0f
Ameya Bhagat <developameya@outlook.com>
2023-10-02 16:23:54 +0530

    add configure dependancies to main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:count_me_in/app.dart';
+import 'package:count_me_in/common/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependancies();
   runApp(const App());
 }
